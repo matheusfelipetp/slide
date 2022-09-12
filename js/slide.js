379a02a262dc6cb4227f35b2b5de14683d16002f
matheusfelipetp <matheusfelipetp@outlook.com>
2022-09-12 16:00:37 -0300

    feat: add slide.js - events touch

---
+++ js/slide.js
@@ -0,0 +1,72 @@
+export class Slide {
+  constructor(slide, wrapper) {
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+    this.distance = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0,
+    };
+  }
+
+  moveSlide(distanceX) {
+    this.distance.movePosition = distanceX;
+    this.slide.style.transform = `translate3d(${distanceX}px, 0,0)`;
+  }
+
+  updatePosition(clientX) {
+    this.distance.movement = (this.distance.startX - clientX) * 1.5;
+    return this.distance.finalPosition - this.distance.movement;
+  }
+
+  onStart(event) {
+    let moveType;
+
+    if (event.type === 'mousedown') {
+      event.preventDefault();
+      this.distance.startX = event.clientX;
+      moveType = 'mousemove';
+    } else {
+      this.distance.startX = event.changedTouches[0].clientX;
+      moveType = 'touchmove';
+    }
+
+    this.wrapper.addEventListener(moveType, this.onMove);
+  }
+
+  onMove(event) {
+    const pointerPosition =
+      event.type === 'mousemove'
+        ? event.clientX
+        : event.changedTouches[0].clientX;
+
+    const finalPosition = this.updatePosition(pointerPosition);
+    this.moveSlide(finalPosition);
+  }
+
+  onEnd(event) {
+    const moveType = event.type === 'mouseup' ? 'mousemove' : 'touchmove';
+
+    this.wrapper.removeEventListener(moveType, this.onMove);
+    this.distance.finalPosition = this.distance.movePosition;
+  }
+
+  addSlideEvents() {
+    this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('touchstart', this.onStart);
+    this.wrapper.addEventListener('mouseup', this.onEnd);
+    this.wrapper.addEventListener('touchend', this.onEnd);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+  }
+
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+    return this;
+  }
+}
